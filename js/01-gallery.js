6668dd1a81d499d5f6f237628c7c932297a4d881
Kinoha12345 <filin.vlad.v@gmail.com>
2021-12-22 18:47:49 +0200

    zczx

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,62 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-// const items = [];
-// const div = document.querySelector('.gallery');
-// const modal = document.querySelector('.modal');
-// const closeBtn = document.querySelector('.closeBtn');
-// const modalImg = document.querySelector('.modalImg');
-// const hidden = document.querySelector('.isHidden')
-
-// for(let i = 0; i < galleryItems.length; i++) {
-//     const item = document.createElement("img");
-//     item.src = galleryItems[i].preview;
-//     item.alt = galleryItems[i].description;
-//     item.classList.add("gallery__item");
-//     item.classList.add("gallery__image");
-//     item.classList.add("gallery__link");
-    
-//     items.push(item);
-// }
-// div.append(...items);
-
-
-
-//     modal.style.position = "absolute";
-//     modal.style.backgroundColor = "black";
-//     modal.style.top = "0";
-//     modal.style.left = "0";
-//     modal.style.right = "0";
-//     modal.style.bottom = "0";
-//     modal.style.width = "100%";
-//     modal.style.height = "100%";
-//     hidden.style.visibility = 'hidden';
-    
-//     closeBtn.addEventListener("click", () => {
-//         modal.classList.add('is_hidden');
-//     })
-
-
-// div.addEventListener("click", fullImg);
-
-// function fullImg(event){
-//     if(event.target.nodeName !== "IMG") {
-//         return false; 
-//     }
-    
-// }
-
 const gallery = document.querySelector('.gallery');
 
 const itemMurkup = galleryItems.map(galleryItems => {
     return `<a class="gallery__item" href="${galleryItems.original}">
-        <img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" />
+        <img class="gallery__image" src="${galleryItems.preview}" data-source="${galleryItems.original}" alt="${galleryItems.description}" />
         </a>`
 }).join('')
 
 gallery.insertAdjacentHTML('afterbegin', itemMurkup);
 
-let lightBox = new SimpleLightbox('.gallery a',{captionDelay:'250ms', captionData:'alt'});
+gallery.addEventListener('click', OpenModel);
+
+function OpenModel(e) {
+    e.preventDefault();
+    if(e.target.tagName !== "IMG") return false;
+    const dataSource = e.target.dataset.source;
+const instance = basicLightbox.create(`
+    <img src="${dataSource}" >
+`)
+
+instance.show()
+}
+
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,53 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
 
-const refs = {
-    galleryEl: document.querySelector(".js-gallery"),
-    lightbox: document.querySelector(".js-lightbox"),
-    lightboxImg: document.querySelector(".lightbox__image"),
-    btn: document.querySelector(".lightbox__button"),
-    lightboxOverlay: document.querySelector(".lightbox__overlay"),
-  };
-  const galleryMarkup = galleryItems
-    .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-      <a
-        class="gallery__link"
-        href="${original}"
-      >
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-     </a>
-     </li> `;
-    })
-    .join("");
-  refs.galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
-  refs.galleryEl.addEventListener("click", openModal);
-  function openModal(e) {
-    e.preventDefault();
-    if (e.target.tagName !== "IMG") return;
-    const dataSource = e.target.dataset.source;
-    console.log(dataSource);
-    // if(e.target.tagName !== "")
-    refs.lightbox.classList.add("is-open");
-    refs.lightboxImg.src = dataSource;
-    document.addEventListener("keydown", closeModalEscape);
-  }
-  refs.btn.addEventListener("click", closeModal);
-  function closeModal(e) {
-    refs.lightbox.classList.remove("is-open");
-    refs.lightboxImg.src = "";
-    document.removeEventListener("keydown", closeModalEscape);
-  }
-  refs.lightboxOverlay.addEventListener("click", closeModal);
-  function closeModalEscape(e) {
-    if (e.code === "Escape") {
-      closeModal();
-    }
-  }
+const gallery = document.querySelector('.gallery');
+
+const itemMurkup = galleryItems.map(galleryItems => {
+    return `<li>
+    <a class="gallery__item" href="${galleryItems.original}">
+        <img class="gallery__image" src="${galleryItems.preview}" data-source="${galleryItems.original}" alt="${galleryItems.description}" />
+        </a>
+        </li>`
+}).join('')
+
+gallery.insertAdjacentHTML('afterbegin', itemMurkup);
+
+
+let lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionData:'alt'});
